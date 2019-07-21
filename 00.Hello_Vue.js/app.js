e06dfb8fdd23231753e98ec70f103772924a2461
youkidkk <youkidkk80@gmail.com>
2019-07-21 11:38:43 +0900

    Hello Vue.js、Directiveを追加

---
+++ 00.Hello_Vue.js/app.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    // elプロパティ
+    // id:app を Vue の管轄とする
+    el: '#app',
+    // dataプロパティ
+    // 指定のキーのデータに値を割り当てる
+    data: {
+        message: 'Hello Vue.js!!!'
+    }
+})
---
+++ 01.Directive/app.js
@@ -0,0 +1,45 @@
+var app = new Vue({
+    // elプロパティ
+    // id:app を Vue の管轄とする
+    el: "#app",
+    // dataプロパティ
+    data: {
+        title: "01.Directive",
+        align: "center",
+        seen: false,
+        items: [{
+                id: 1,
+                text: "Item 1"
+            },
+            {
+                id: 2,
+                text: "Item 2"
+            },
+            {
+                id: 3,
+                text: "Item 3"
+            }
+        ],
+        newItem: {
+            id: 4,
+            text: ""
+        }
+    },
+    // methodsプロパティ
+    methods: {
+        switchSeen: function () {
+            this.seen = !this.seen
+        },
+        addItem: function () {
+            if (this.newItem.text == "") {
+                return
+            }
+            this.items.push({
+                id: this.newItem.id,
+                text: this.newItem.text
+            })
+            this.newItem.id = this.newItem.id + 1
+            this.newItem.text = ""
+        }
+    }
+})
